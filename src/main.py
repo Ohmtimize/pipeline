01b6e9c3a963a40f1c1d8fdb69b66d5d71b8b5bb
Lucius Caesar <lucien.culot@gmail.com>
2024-04-26 16:06:08 +0200

    Change to loop_forever

--- src/main.py
+++ src/main.py
@@ -10,63 +10,61 @@ load_dotenv()
 # Define topic
 topic = "#"
 
+
 # Connect functions
 def on_connect(client, userdata, flags, rc, properties=None):
     if rc == 0:
-         print("CONNACK received with result code %s." % rc)
+        print("CONNACK received with result code %s." % rc)
     else:
-        print("Failed to connect, return code %d\n", rc)        
+        print("Failed to connect, return code %d\n", rc)
 
 
 def connect_mqtt():
-        # Create client instance
-        client = paho.Client(
+    # Create client instance
+    client = paho.Client(
         callback_api_version=paho.CallbackAPIVersion.VERSION2,
         client_id="2",
         userdata=None,
         protocol=paho.MQTTv5,
-        )
+    )
 
-        client.on_connect = on_connect
-        client.connect(os.getenv("MQTT_HOST"), int(os.getenv("PORT"))) # client.connect(broker, port)
-        return client
-     
+    client.on_connect = on_connect
+    client.connect(
+        os.getenv("MQTT_HOST"), int(os.getenv("PORT"))
+    )  # client.connect(broker, port)
+    return client
+
 
 # Print message - callback for when a publish message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
     if not os.path.exists("data/"):
-         os.makedirs("data/")
+        os.makedirs("data/")
     with open("data/msg.txt", "a+") as file:
         file.write(msg.topic + " " + str(msg.qos) + " " + str(msg.payload) + "\n")
 
+
 # Subscribe function
 def subscribe(client, topic):
-    client.subscribe(topic, qos=1) # subscribe to all topics
+    client.subscribe(topic, qos=1)  # subscribe to all topics
     client.on_message = on_message
+
 
 # Main function
 def main():
-        # Program starts
-        print("Connecting to client...")
+    # Program starts
+    print("Connecting to client...")
 
-        # Connect and subscribe
-        client = connect_mqtt()
-        subscribe(client, topic)
-        
-        start_time = time.time()
-        duration = 15 # seconds
-
-            
-        # Disconnect the client from the broker after X seconds (duration)
-        while time.time() - start_time <= duration:
-            rc = client.loop(timeout=1.0)
-            if rc !=0:
-                print("Connection error, please reconnect")
-                break
-
-        print("Disconnecting...")
+    # Connect and subscribe
+    client = connect_mqtt()
+    subscribe(client, topic)
+    try:
+        while True:
+            client.loop_forever()
+    except KeyboardInterrupt:
         client.disconnect()
+        print("Disconnecting from client...")
+
 
 if __name__ == "__main__":
     main()
