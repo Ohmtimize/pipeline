713ae0222a3aaccf783b59ee8559ca510b624316
Lucius Caesar <lucien.culot@gmail.com>
2024-04-30 16:53:23 +0200

    Add DB Trigger to format the Database

--- README.md
+++ README.md
@@ -70,9 +70,33 @@ docker container exec mosquitto mosquitto_sub -i mosq_sub1 -t "#"
 In the second one, you can publish a message by running:
 
 ```zsh
-docker container exec mosquitto mosquitto_pub -t bedroom/temperature -m "bedroom_temperature celsius=20"
+docker container exec mosquitto mosquitto_pub -t base_topic/domain/entity/state -m "this is a value"
 ```
 
 ## Usefull links
 
 - tutorial to create mqtt service into the docker-compose file: <https://medium.com/@agalliani/what-is-mqtt-what-is-a-broker-and-how-to-deploy-it-with-docker-a2120b918741>
+
+## Pipeline documentation
+
+1. The HomeAssistant publishes on an MQTT server with the MQTT Statestream plugin.  Which publishes every state changes according to the documentation: <https://www.home-assistant.io/integrations/mqtt_statestream/>.
+Note that the topics are structured this way: `base_topic/domain/entity/state`, for example: `homeassistant/light/master_bedroom_dimmer/state`
+2. Our pipeline subscribes to the MQTT broker and saves every messages into a MySQL database in the table `messages`.
+3. A DB Trigger runs after insert to copy the information into tables structured in such a way that it is easy to query values from defined entities, etc.
+For example, you can use the following query to retrieve all values:
+
+```SQL
+select
+    facts.id,
+    facts.fact,
+    states.label as state,
+    states.id as state_id,
+    entities.label as entity,
+    entities.id as entity_id,
+    base_topics.label as main_topic,
+    base_topic_id as main_topic_id
+from facts
+join states on facts.state_id = states.id
+join entities on states.entity_id = entities.id
+join base_topics on entities.base_topic_id = base_topics.id
+```
--- migrations/migrate.py
+++ migrations/migrate.py
@@ -11,10 +11,12 @@ sys.path.append("src")
 from mysqlDriver import MysqlDB
 
 # Create migration table if does not yet exist
-TABLES = {}
+QUERIES = {}
 
-TABLES["migrations"] = {
-    "Create table": "CREATE TABLE migrations ("
+STATEMENT = "Create table"
+
+QUERIES["migrations"] = {
+    STATEMENT: "CREATE TABLE migrations ("
     "id int(1) NOT NULL AUTO_INCREMENT,"
     "name varchar(255) NOT NULL,"
     "inserted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
@@ -23,8 +25,8 @@ TABLES["migrations"] = {
     ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
 }
 
-TABLES["messages"] = {
-    "Create table": "CREATE TABLE messages ("
+QUERIES["messages"] = {
+    STATEMENT: "CREATE TABLE messages ("
     "id int(11) NOT NULL AUTO_INCREMENT,"
     "topic varchar(255) NOT NULL,"
     "qos int(11) NOT NULL,"
@@ -35,18 +37,102 @@ TABLES["messages"] = {
     ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
 }
 
+QUERIES["base_topics"] = {
+    STATEMENT: "CREATE TABLE base_topics ("
+    "id int(11) NOT NULL AUTO_INCREMENT,"
+    "label varchar(255) NOT NULL,"
+    "inserted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+    "last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+    "PRIMARY KEY (id),"
+    "UNIQUE KEY (label)"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+}
+
+QUERIES["entities"] = {
+    STATEMENT: "CREATE TABLE entities ("
+    "id int(11) NOT NULL AUTO_INCREMENT,"
+    "label varchar(255) NOT NULL,"
+    "base_topic_id int(11) NOT NULL,"
+    "inserted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+    "last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+    "PRIMARY KEY (id),"
+    "UNIQUE KEY (base_topic_id, label),"
+    "FOREIGN KEY (base_topic_id) REFERENCES base_topics(id)"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+}
+
+QUERIES["states"] = {
+    STATEMENT: "CREATE TABLE states ("
+    "id int(11) NOT NULL AUTO_INCREMENT,"
+    "entity_id int(11) NOT NULL,"
+    "label varchar(255) NOT NULL,"
+    "inserted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+    "last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+    "PRIMARY KEY (id),"
+    "UNIQUE KEY (entity_id, label),"
+    "FOREIGN KEY (entity_id) REFERENCES entities(id)"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+}
+
+QUERIES["domains"] = {
+    STATEMENT: "CREATE TABLE domains ("
+    "id int(11) NOT NULL AUTO_INCREMENT,"
+    "entity_id int(11) NOT NULL,"
+    "label varchar(255) NOT NULL,"
+    "inserted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+    "last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+    "PRIMARY KEY (id),"
+    "UNIQUE KEY (entity_id, label),"
+    "FOREIGN KEY (entity_id) REFERENCES entities(id)"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+}
+
+QUERIES["facts"] = {
+    STATEMENT: "CREATE TABLE facts ("
+    "id int(11) NOT NULL AUTO_INCREMENT,"
+    "state_id int(11) NOT NULL,"
+    "fact varchar(255) NOT NULL,"
+    "inserted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+    "last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+    "PRIMARY KEY (id),"
+    "FOREIGN KEY (state_id) REFERENCES states(id)"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+}
+
+QUERIES["trigger"] = {
+    STATEMENT: "CREATE TRIGGER formating "
+    "AFTER INSERT "
+    "ON messages FOR EACH ROW "
+    "BEGIN "
+    "INSERT INTO base_topics (label) "
+    "SELECT substring_index(substring_index(NEW.topic, '/', 1), '/', -1) as label "
+    "ON DUPLICATE KEY UPDATE label=label; "
+    "INSERT INTO entities (label, base_topic_id) "
+    "SELECT substring_index(substring_index(NEW.topic, '/', 3), '/', -1) AS label, base_topics.id AS base_topic_id FROM base_topics "
+    "WHERE base_topics.label=substring_index(substring_index(NEW.topic, '/', 1), '/', -1) "
+    "ON DUPLICATE KEY UPDATE entities.label=entities.label; "
+    "INSERT INTO states (label, entity_id) "
+    "SELECT substring_index(substring_index(NEW.topic, '/', 4), '/', -1) AS label, entities.id AS entity_id FROM entities "
+    "WHERE entities.label=substring_index(substring_index(NEW.topic, '/', 3), '/', -1) "
+    "ON DUPLICATE KEY UPDATE states.label=states.label; "
+    "INSERT INTO facts (fact, state_id) "
+    "SELECT NEW.payload AS fact, states.id AS state_id FROM states "
+    "WHERE states.label=substring_index(substring_index(NEW.topic, '/', 4), '/', -1); "
+    "END;"
+}
+
 
 def main():
     with MysqlDB() as db:
-        for table_name in TABLES:
-            table_description = TABLES[table_name]
+        for table_name in QUERIES:
+            table_description = QUERIES[table_name]
             try:
-                print("Creating table {}: ".format(table_name), end="")
-                db.runSQL(table_description["Create table"], None)
+                print("Running Queries {}: ".format(table_name), end="")
+                db.runSQL(table_description[STATEMENT], None)
                 print("OK")
             except db.connector.Error as err:
                 if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                    print("already exists.")
+                    print("table already exists.")
                 else:
                     print(err.msg)
 
